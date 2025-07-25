f9889e60554d97cbe66b251667d423d738dfda43
zacksarada <hamandongotongh@gmail.com>
2025-07-25 11:58:25 +0700

    Update main.py
    from fastapi import FastAPI
    from pydantic import BaseModel
    from utils import load_model, predict_prices
    
    app = FastAPI()
    
    class PredictRequest(BaseModel):
        coin: str
        days: int
    
    @app.post("/predict")
    def predict(req: PredictRequest):
        coin = req.coin.upper()
        model = load_model(coin)
        result = predict_prices(model, req.days)
        return {"predicted_price": result}

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from utils import load_model, predict_prices
 
+app = FastAPI()
+
+class PredictRequest(BaseModel):
+    coin: str
+    days: int
+
+@app.post("/predict")
+def predict(req: PredictRequest):
+    coin = req.coin.upper()
+    model = load_model(coin)
+    result = predict_prices(model, req.days)
+    return {"predicted_price": result}
