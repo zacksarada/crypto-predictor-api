52e630b49a7d818f1bbad729d1bfea57fd4dade3
zacksarada <hamandongotongh@gmail.com>
2025-07-25 11:59:56 +0700

    Update utils.py
    import pickle
    import numpy as np
    
    def load_model(coin):
        folder = "model/"
        with open(f"{folder}{coin}_model.pkl", "rb") as f:
            model = pickle.load(f)
        return model
    
    def predict_prices(model, days):
        x = np.arange(days).reshape(-1, 1)
        preds = model.predict(x)
        return preds.tolist()

--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
 
+import pickle
+import numpy as np
+
+def load_model(coin):
+    folder = "model/"
+    with open(f"{folder}{coin}_model.pkl", "rb") as f:
+        model = pickle.load(f)
+    return model
+
+def predict_prices(model, days):
+    x = np.arange(days).reshape(-1, 1)
+    preds = model.predict(x)
+    return preds.tolist()
